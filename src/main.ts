78727bce7bd1f3ead38fbbe1779e09cec3f8a8d7
xushengfeng <xushengfeng_zg@163.com>
2024-02-07 00:10:36 +0800

    画布 移动元素

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { el } from "redom";
+import { el, setStyle } from "redom";
 
 type aiM = {
     role: "system" | "user" | "ai";
@@ -6,10 +6,8 @@ type aiM = {
 };
 let aim: Map<string, aiM> = new Map();
 
-type graphNode = Map<
-    string,
-    { parents: string[]; children: string[]; posi: { x: number; y: number; w: number; h: number } }
->;
+type rect = { x: number; y: number; w: number; h: number };
+type graphNode = Map<string, { parents: string[]; children: string[]; posi: rect }>;
 let graph: graphNode = new Map();
 
 let inputId = "0";
@@ -107,11 +105,75 @@ function setData(id: string) {
         ]
     );
     if (data.content.image) cardEl.appendChild(el("img", data.content.image.src));
-    let oldEl = contextEl.querySelector(`[data-id="${id}"]`);
+    let oldEl = getCard(id);
     if (oldEl) {
         contextEl.replaceChild(cardEl, oldEl);
     } else {
         contextEl.appendChild(cardEl);
+        addHandle(cardEl);
+    }
+}
+
+function getCard(id: string) {
+    return contextEl.querySelector(`[data-id="${id}"]`);
+}
+
+function addHandle(i: HTMLElement) {
+    let x_h = [];
+
+    for (let i = 0; i < 8; i++) {
+        x_h[i] = el("div", { class: `xxhandle${i}` });
+    }
+    i.append(el("div", { class: "haddle" }, x_h));
+
+    i.onpointerdown = (e) => {
+        let el = e.target as HTMLDivElement;
+        if (x_h.includes(el)) {
+            e.preventDefault();
+            e.stopPropagation();
+            free_o_a = x_h.indexOf(el);
+            free_old_point = e2p(e);
+            free_o_rects = graph.get(i.getAttribute("data-id")).posi;
+        } else {
+            if (e.ctrlKey) {
+                free_o_a = -1;
+                free_old_point = e2p(e);
+                free_o_rects = graph.get(i.getAttribute("data-id")).posi;
+            }
+        }
+    };
+}
+
+/** 框有交集 */
+function rect_x_rect(rect0: rect, rect1: rect) {
+    if (
+        rect0.x <= rect1.x + rect1.w &&
+        rect0.x + rect0.w >= rect1.x &&
+        rect0.y <= rect1.y + rect1.h &&
+        rect0.y + rect0.h >= rect1.y
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function reflashNode() {
+    let outRect = { x: 0, y: 0, w: window.innerWidth, h: window.innerHeight };
+    for (let i of graph) {
+        const id = i[0];
+        const rect = i[1].posi;
+        const _rect = structuredClone(rect);
+        _rect.x = rect.x + op.x;
+        _rect.y = rect.y + op.y;
+        if (rect_x_rect(outRect, rect)) {
+            let el = getCard(id);
+            if (el) {
+                setStyle(el, { left: rect.x + "px", top: rect.y + "px", width: rect.w + "px", height: rect.h + "px" });
+            } else {
+                setData(id);
+            }
+        }
     }
 }
 
@@ -211,6 +273,7 @@ function set_O_p(x: number | null, y: number | null, dx?: number | null, dy?: nu
         contextEl.style.top = y + "px";
         op.y = y;
     }
+    reflashNode();
 }
 
 contextElP.onwheel = (e) => {
@@ -257,3 +320,153 @@ document.addEventListener("pointerup", () => {
     }
     middle_b = null;
 });
+
+let free_old_point: p_point;
+/** 所作用的元素及其原始位置大小 */
+let free_o_rects: rect;
+/** 应该对元素执行的操作，移动还是调节大小 */
+let free_o_a = NaN;
+/** 是否移动过，可用于判断点击还是拖动 */
+let free_move = false;
+
+var changePosiId = "0";
+
+function e2p(e: MouseEvent) {
+    return {
+        x: e.clientX - el_offset(contextEl, document.body).x,
+        y: e.clientY - el_offset(contextEl, document.body).y,
+    } as p_point;
+}
+var MouseEvent = (e: MouseEvent) => {
+    if (!free_old_point) return;
+    let xel = free_o_rects;
+    let np = e2p(e);
+    let dx = np.x - free_old_point.x,
+        dy = np.y - free_old_point.y;
+    let w = xel.w,
+        h = xel.h;
+
+    switch (free_o_a) {
+        case -1:
+            c(-10, dx);
+            c(-11, dy);
+            break;
+        case 0:
+            c(0, -dy);
+            break;
+        case 1:
+            c(1, dx);
+            break;
+        case 2:
+            c(2, dy);
+            break;
+        case 3:
+            c(3, -dx);
+            break;
+        case 4:
+            // ↗
+            if (e.shiftKey) {
+                let j = dx * w - dy * h;
+                c(0, (j * h) / (w ** 2 + h ** 2));
+                c(1, (j * w) / (w ** 2 + h ** 2));
+            } else {
+                c(0, -dy);
+                c(1, dx);
+            }
+            break;
+        case 5:
+            // ↘
+            if (e.shiftKey) {
+                let j = dx * w + dy * h;
+                c(1, (j * w) / (w ** 2 + h ** 2));
+                c(2, (j * h) / (w ** 2 + h ** 2));
+            } else {
+                c(1, dx);
+                c(2, dy);
+            }
+            break;
+        case 6:
+            // ↙
+            if (e.shiftKey) {
+                let j = -dx * w + dy * h;
+                c(2, (j * h) / (w ** 2 + h ** 2));
+                c(3, (j * w) / (w ** 2 + h ** 2));
+            } else {
+                c(2, dy);
+                c(3, -dx);
+            }
+            break;
+        case 7:
+            // ↖
+            if (e.shiftKey) {
+                let j = -dx * w - dy * h;
+                c(3, (j * w) / (w ** 2 + h ** 2));
+                c(0, (j * h) / (w ** 2 + h ** 2));
+            } else {
+                c(3, -dx);
+                c(0, -dy);
+            }
+            break;
+    }
+    function c(a: number, d: number) {
+        xel = changePosi(xel, e, dx, dy, a, d);
+    }
+    graph.get(changePosiId).posi = xel;
+    reflashNode();
+};
+
+contextElP.addEventListener("pointermove", MouseEvent);
+contextElP.addEventListener("pointerup", (e) => {
+    free_old_point = null;
+    MouseEvent(e);
+});
+
+function changePosi(
+    xel: { x: number; y: number; w: number; h: number },
+    e: MouseEvent,
+    dx: number,
+    dy: number,
+    a: number,
+    d: number
+) {
+    let x = NaN,
+        y = NaN,
+        w = NaN,
+        h = NaN;
+    let i = 1,
+        j = 0;
+    if (e.ctrlKey) {
+        i = 2;
+        j = 1;
+    }
+    switch (a) {
+        case -10:
+            x = xel.x + dx;
+            break;
+        case -11:
+            y = xel.y + dy;
+            break;
+        case 0:
+            y = xel.y - d;
+            h = xel.h + i * d;
+            break;
+        case 1:
+            w = xel.w + i * d;
+            x = xel.x - j * d;
+            break;
+        case 2:
+            h = xel.h + i * d;
+            y = xel.y - j * d;
+            break;
+        case 3:
+            x = xel.x - d;
+            w = xel.w + i * d;
+            break;
+    }
+    return {
+        x: isNaN(x) ? xel.x : x,
+        y: isNaN(y) ? xel.y : y,
+        w: isNaN(w) ? xel.w : w,
+        h: isNaN(h) ? xel.h : h,
+    };
+}
